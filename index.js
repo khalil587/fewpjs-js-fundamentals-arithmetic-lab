c0fc8de32ad3493d9d9c460914e4c34682e79100
Khalil Lindsay <lindsay.khalil@gmail.com>
2021-05-18 20:48:24 -0400

    commit tree db42479aa1cbd50806df793328548401e8205e72

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+  let random = 10;
+  Math.random();
+  
+  let num3 = 4;
+  let num4 = 5;
+  var mod = num3 % num4;
 
-// Write your code here
+  let max = (10,11,12,13,14,15,16,17,18,19,20);
+  Math.max;
